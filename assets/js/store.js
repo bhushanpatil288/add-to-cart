8a0872614d339836d57b25d6ca63d80203ca3f0d
bhushanpatil288 <sharewithbhushan@gmail.com>
2025-12-29 14:43:21 +0530

    [modify] - [add-to-cart-behaviour] - now clicking on add to cart makes it added to cart and clicking agian removes it, persistent state even after reload of page

--- assets/js/store.js
+++ assets/js/store.js
@@ -260,30 +260,63 @@ function loadLocalStorage(){
 }
 
 function renderProducts(products){
+    const cart = loadLocalStorage();
     toyCardsContainer.innerHTML = "";
+    let isAlreadyInCart = false;
     products.forEach((product,idx)=>{
-        toyCardsContainer.innerHTML += `
-        <div class="col-12 col-md-6 col-xl-4 gy-4">
-            <div class="toy-card bg-white px-3 py-2 rounded-4 shadow">
-              <div class="overflow-hidden">
-                <img class="img-fluid" src="${product.path}" alt="${product.name}">
-              </div>
-              <div class="d-flex justify-content-around flex-column">
-                <div>
-                    <p class="category">${product.category}</p>
-                    <h3>${product.name}</h3>    
-                    <p class="price text-primary-custom fs-2 fw-semibold m-0">$${(product.price).toFixed(2)}</p>
+        if (cart.some(item=>item.id === product.id)){
+          isAlreadyInCart = true;
+        } else {
+          isAlreadyInCart = false;
+        }
+        if(!isAlreadyInCart){
+          toyCardsContainer.innerHTML += `
+          <div class="col-12 col-md-6 col-xl-4 gy-4">
+              <div class="toy-card bg-white px-3 py-2 rounded-4 shadow">
+                <div class="overflow-hidden">
+                  <img class="img-fluid" src="${product.path}" alt="${product.name}">
                 </div>
-                <div class="d-flex justify-content-center align-items-center gap-3">
-                  <button onClick="addToCart(${product.id})" class="add-to-cart-btn d-flex justify-content-center align-items-center gap-1 mt-3 mb-2">
-                    <span class="fs-5"><i class="ri-shopping-bag-4-line"></i></span>
-                    <p class="m-0">Add to cart</p>
-                  </button>
+                <div class="d-flex justify-content-around flex-column">
+                  <div>
+                      <p class="category">${product.category}</p>
+                      <h3>${product.name}</h3>    
+                      <p class="price text-primary-custom fs-2 fw-semibold m-0">$${(product.price).toFixed(2)}</p>
+                  </div>
+                  <div class="d-flex justify-content-center align-items-center gap-3">
+                    <button onClick="addToCart(${product.id})" class="add-to-cart-btn add-to-cart-btn-${product.id} d-flex justify-content-center align-items-center gap-1 mt-3 mb-2">
+                      <span class="fs-5"><i class="ri-shopping-bag-4-line"></i></span>
+                      <p class="m-0">Add to cart</p>
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-      `
+        `
+        }
+        else {
+          toyCardsContainer.innerHTML += `
+          <div class="col-12 col-md-6 col-xl-4 gy-4">
+              <div class="toy-card bg-white px-3 py-2 rounded-4 shadow">
+                <div class="overflow-hidden">
+                  <img class="img-fluid" src="${product.path}" alt="${product.name}">
+                </div>
+                <div class="d-flex justify-content-around flex-column">
+                  <div>
+                      <p class="category">${product.category}</p>
+                      <h3>${product.name}</h3>    
+                      <p class="price text-primary-custom fs-2 fw-semibold m-0">$${(product.price).toFixed(2)}</p>
+                  </div>
+                  <div class="d-flex justify-content-center align-items-center gap-3">
+                    <button onClick="addToCart(${product.id})" class="add-to-cart-btn add-to-cart-btn-${product.id} d-flex justify-content-center align-items-center gap-1 mt-3 mb-2">
+                      <span class="fs-5"><i class="ri-checkbox-circle-fill"></i></span>
+                      <p class="m-0">Added to cart</p>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+        `
+        }
     })
 }
 
@@ -298,18 +331,21 @@ function updateBadge(){
 }
 
 function addToCart(id){
+  const clickedBtn = document.querySelector(`.add-to-cart-btn-${id}`);
   const cart = loadLocalStorage();
   if(alreadyExists(id)){
-    const idx = cart.findIndex(product => product.id === id);
-    cart[idx].quantity++;
-
+    clickedBtn.innerHTML = `<span class="fs-5"><i class="ri-shopping-bag-4-line"></i></span>
+                            <p class="m-0">Add to cart</p>`;
+    const idx = cart.findIndex(item => item.id === id);                       
+    cart.splice(idx, 1);
     saveLocalStorage(cart);
-
     updateBadge();
     return;
   }
   const product = products.find(p => id === p.id);
   cart.push({...product, quantity: 1});
+  clickedBtn.innerHTML = `<span class="fs-5"><i class="ri-checkbox-circle-fill"></i></span>
+                          <p class="m-0">Added to cart</p>`;
   saveLocalStorage(cart);
   updateBadge();
 }
